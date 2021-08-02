fc52c6786649354b5b73144855ee68d3fb7546bd
rudaks <rudaks94@gmail.com>
2021-08-02 21:37:51 +0900

    update

---
+++ udemy/tasky/index.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const electron = require('electron');
+const TimerTray = require('./app/timer_tray');
+
+const { app, BrowserWindow, Tray } = electron;
+
+let mainWindow;
+
+let tray;
+
+app.on('ready', () => {
+    app.dock.hide();
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation : false
+        },
+        height: 500,
+        width: 300,
+        frame: false,
+        resizable: false,
+        show: false
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    mainWindow.on('blur', () => {
+       mainWindow.hide();
+    });
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png': 'iconTemplate.png';
+    const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+    tray = new TimerTray(iconPath, mainWindow);
+})
