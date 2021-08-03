7984c2ee5856adf9a4b83c831ac4fd23471d843f
rudaks <rudaks94@gmail.com>
2021-08-03 10:46:16 +0900

    update

--- udemy/tasky/index.js
+++ udemy/tasky/index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const electron = require('electron');
 const TimerTray = require('./app/timer_tray');
+const MainWindow = require('./app/main_window');
 
-const { app, BrowserWindow, Tray } = electron;
+const { app, ipcMain } = electron;
 
 let mainWindow;
 
@@ -10,23 +11,13 @@ let tray;
 
 app.on('ready', () => {
     app.dock.hide();
-    mainWindow = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation : false
-        },
-        height: 500,
-        width: 300,
-        frame: false,
-        resizable: false,
-        show: false
-    });
-    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-    mainWindow.on('blur', () => {
-       mainWindow.hide();
-    });
+    mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png': 'iconTemplate.png';
     const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
     tray = new TimerTray(iconPath, mainWindow);
-})
+});
+
+ipcMain.on("update-timer", (event, timerLeft) => {
+   tray.setTitle(timerLeft);
+});
