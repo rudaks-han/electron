e47869e63ff3edde60d121f7fd952de8b6b971c5
rudaks <kmhan@spectra.co.kr>
2021-08-20 11:29:00 +0900

    update

--- my-dashboard/js/component.js
+++
@@ -1,5 +0,0 @@
-require('./component/daouoffice.js');
-require('./component/jira.js');
-require('./component/jenkins.js');
-require('./component/sonarqube.js');
-require('./component/victoryPortal.js');
